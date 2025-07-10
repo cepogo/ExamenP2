990bab1be4d02f81a0bccdec75c7ad0dc59c8ee5
cepogo <cepogo@espe.edu.ec>
2025-07-09 19:38:45 -0500

    primer comit

---
+++ turno/src/main/java/ec/edu/espe/turno/TurnoApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.turno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnoApplication.class, args);
+	}
+
+}
---
+++ turno/src/test/java/ec/edu/espe/turno/TurnoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.turno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
